01caa60a4c7c107e0362e14a5e1de0e6543922b1
Ashish Maroti Kale <akale1992@gmail.com>
2017-04-15 17:39:59 +0530

    Add files via upload

--- src/com/ashish/hibernate/HibernateTest.java
+++ src/com/ashish/hibernate/HibernateTest.java
@@ -10,26 +10,33 @@ public class HibernateTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		/*Person person = new Person();
+		Person person = new Person();
 		person.setId(1);
 		person.setFirstName("Ashish");
 		person.setLastName("Kale");
-		person.setSalary(10000.50);*/
+		person.setSalary(10000.50);
 		
 		Person person1 = new Person();
 		person1.setId(2);
 		person1.setFirstName("Someshwar");
 		person1.setLastName("Kale");
-		person1.setSalary(2000.75);
+		person1.setSalary(122000.75);
 		
 		
 		SessionFactory factory = new Configuration().configure().buildSessionFactory();
 		Session session = factory.openSession();
 		session.beginTransaction();
-	//	session.persist(person);
+		session.persist(person);
 		session.save(person1);
 		session.getTransaction().commit();
 		session.close();
+		
+	
+		
+		Session session1 = factory.openSession();
+		session1.beginTransaction();
+		Person person2 = (Person) session1.get(Person.class, 2);
+		System.out.println("Person name is "+person2.getFirstName());
 
 	}
 
